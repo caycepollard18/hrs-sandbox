1e159e537dee16501c590b00673489516f8a32f5
Cayce Pollard <caycepollard18@gmail.com>
2021-07-19 12:21:51 -0700

    Adding Google Analytics gtag.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { Head, Layout } from '@components/common'
 import { ManagedUIContext } from '@components/ui/context'
+import * as analytics from '@lib/analytics'
+import { useRouter } from 'next/router'
 import React, { useEffect } from 'react'
 import theme from '@theme'
 import { ThemeProvider } from 'theme-ui'
@@ -8,6 +10,23 @@ import { ThemeProvider } from 'theme-ui'
 import '@styles/fonts.css'
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      analytics.pageview(url)
+    }
+
+    // While the component is mounted, subscribe to router changes
+    // and log those as pageviews in Analytics
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // When the component unmounts, unsubscribe from router
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   useEffect(() => {
     document.body.classList?.remove('loading')
   }, [])
--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,24 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+          {/* End Global Site Tag (gtag.js) - Google Analytics */}
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
