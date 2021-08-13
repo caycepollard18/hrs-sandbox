2ebaf0701de4421beb2577237f77a723bbd88f34
Cayce Pollard <caycepollard18@gmail.com>
2021-08-13 12:06:34 -0700

    100vh iPhone bug fix attempt

--- components/ui/Sidebar.js
+++ components/ui/Sidebar.js
@@ -60,7 +60,7 @@ const Sidebar = ({ children, open, onClose }) => {
             className="test-name"
             ref={ref}
             sx={{
-              width: '100%', height: '100vh',
+              width: '100%', height: '100%',
               overflowX: 'hidden',
               overflowY: 'hidden',
               position: 'absolute',
